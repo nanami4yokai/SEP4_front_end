b76414d09f91169e34139970b221e1ee8b5e0b10
Aiga <aiga.kalneja@gmail.com>
2023-05-02 20:12:42 +0200

    Simple temp display

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className= "container">
+        <div className="temp">
+          <h1>25º</h1>
+        </div>
+        <div className="description" >
+          <p> Current temperature</p>
+        </div>
+      </div>
+      
     </div>
   );
 }
