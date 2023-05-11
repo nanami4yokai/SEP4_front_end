8f0b17e57f13fc55c46b18d6c3ba6c16f3b77325
Aiga <aiga.kalneja@gmail.com>
2023-05-11 13:27:46 +0200

    Deleted Proof of Concept files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import TempMock from './components/tempMock';
 import Sidebar from './components/sideBar';
 import TempDisplay from './components/TempDisplay';
 import CO2Display from './components/CO2Display';
@@ -8,7 +7,6 @@ import HumidDisplay from './components/HumidDisplay';
 function App() {
   return (
     <div className="App">
-      {/* <TempMock /> */}
       <Sidebar />
       <TempDisplay />
       <CO2Display/>
