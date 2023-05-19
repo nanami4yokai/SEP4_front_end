1c693ecc6656512ca639c030ed9d97d7b4c1b383
Diana Antoniuc <diana.antoniuc@gmail.com>
2023-05-19 16:47:12 +0200

    hotfix: Add functionality to filters

--- src/components/tempGraph.js
+++ src/components/tempGraph.js
@@ -1,23 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import { Chart as ChartJS, LineElement, CategoryScale, PointElement, LinearScale } from 'chart.js'
-import { Line } from 'react-chartjs-2'
+import React, { useState, useEffect } from 'react';
+import { Chart as ChartJS, LineElement, CategoryScale, PointElement, LinearScale } from 'chart.js';
+import { Line } from 'react-chartjs-2';
 import axios from 'axios';
-// import myData from '../data/recordings-data.json' mockup data source
-import "./TempGraph.css"
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
-import DropdownMenu from 'react-bootstrap/esm/DropdownMenu';
-
+import "./TempGraph.css";
+import DataFilter from './filter';
 
 ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement);
 
 const TempGraph = () => {
   const [chartData, setChartData] = useState(null);
+  const [filterOption, setFilterOption] = useState('realtime');
+  const [dataRange, setDataRange] = useState(24); 
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://terrasense-service-dot-terrasense.ew.r.appspot.com/reading/?start=2020-01-01%2000:00:00&end=2024-01-01%2000:00:00');
+        let url = 'https://terrasense-service-dot-terrasense.ew.r.appspot.com/reading/?start=2020-01-01%2000:00:00&end=2024-01-01%2000:00:00';
+
+        if (filterOption === 'daily') {
+          // Logic to filter by day
+          const today = new Date();
+          const startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0);
+          const endDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1, 0, 0, 0);
+          url += `&start=${startDate.toISOString()}&end=${endDate.toISOString()}`;
+          setDataRange(24);
+        } else if (filterOption === 'weekly') {
+          // Logic to filter by week
+          const today = new Date();
+          const startOfWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - today.getDay(), 0, 0, 0);
+          const endOfWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - today.getDay() + 7, 0, 0, 0);
+          url += `&start=${startOfWeek.toISOString()}&end=${endOfWeek.toISOString()}`;
+          setDataRange(168);
+        } else if (filterOption === 'monthly') {
+          // Logic to filter by month
+          const today = new Date();
+          const startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1, 0, 0, 0);
+          const endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1, 0, 0, 0);
+          url += `&start=${startOfMonth.toISOString()}&end=${endOfMonth.toISOString()}`;
+          setDataRange(720);
+        }
+
+        const response = await axios.get(url);
         const readings = response.data;
         const temperatureData = readings.map((element) => ({
           temperature: element.temperature,
@@ -30,14 +53,29 @@ const TempGraph = () => {
     };
 
     fetchData();
-  }, []);
+  }, [filterOption]);
+
+  const filterDataByOption = (data) => {
+    if (filterOption === 'realtime') {
+      return data.map((element) => element.temperature);
+    } else if (filterOption === 'daily') {
+      // Filter last day's data
+      return data.slice(-dataRange).map((element) => element.temperature);
+    } else if (filterOption === 'weekly') {
+      // Filter last week's data
+      return data.slice(-dataRange).map((element) => element.temperature);
+    } else if (filterOption === 'monthly') {
+      // Filter last month's data
+      return data.slice(-dataRange).map((element) => element.temperature);
+    }
+  };
 
   const data = {
-    labels: chartData ? chartData.map((element) => element.timestamp) : [],
+    labels: chartData ? chartData.slice(-dataRange).map((element) => element.timestamp) : [],
     datasets: [
       {
         label: 'max alert',
-        data: [28, 28, 28, 28, 28, 28],
+        data: Array(dataRange).fill(28),
         fill: false,
         backgroundColor: 'red',
         borderColor: 'red',
@@ -45,14 +83,14 @@ const TempGraph = () => {
       },
       {
         label: 'temperature',
-        data: chartData ? chartData.map((element) => element.temperature) : [],
+        data: chartData ? filterDataByOption(chartData) : [],
         backgroundColor: 'rgba(000, 000, 000, 1)',
         borderColor: 'rgba(000, 000, 000, 1)',
         borderWidth: 1
       },
       {
         label: 'min alert',
-        data: [18, 18, 18, 18, 18, 18],
+        data: Array(dataRange).fill(18),
         fill: false,
         backgroundColor: 'blue',
         borderColor: 'blue',
@@ -106,17 +144,11 @@ const TempGraph = () => {
           </div>
         </div>
         <div className='filter'>
-          <Dropdown>
-            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-            </Dropdown.Toggle>
-            <Dropdown.Menu variant='dark'>
-              <Dropdown.Item href="#/action-1" active>Real time</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item href="#/action-2">Daily</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Weekly</Dropdown.Item>
-              <Dropdown.Item href="#/action-4">Monthly</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          <DataFilter
+            filterOption={filterOption}
+            setFilterOption={setFilterOption}
+            setDataRange={setDataRange}
+          />
         </div>
       </div>
       <br />
@@ -136,103 +168,3 @@ const TempGraph = () => {
 };
 
 export default TempGraph;
-
-
-// Code for working with Mockup data
-
-// ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement)
-
-// const TempGraph = () => {
-//     const [chart, setChart] = useState([]);
-
-//     useEffect(() => {
-//         setChart(myData.readings);
-//     }, [])
-
-//     var chartData = {
-//         labels: chart?.map(element => element.id),
-//         datasets: [
-//             {
-//                 label: 'max alert',
-//                 data: [28, 28, 28, 28, 28],
-//                 fill: false,
-//                 backgroundColor: 'red',
-//                 borderColor: 'red',
-//                 borderWidth: 1
-//             },
-//             {
-//                 label: 'temperature',
-//                 data: chart?.map(element => element.temperature),
-//                 backgroundColor: ['rgba(000, 000, 000, 1)'],
-//                 borderColor: ['rgba(000, 000, 000, 1)'],
-//                 borderWidth: 1
-//             },
-//             {
-//                 label: 'min alert',
-//                 data: [18, 18, 18, 18, 18],
-//                 fill: false,
-//                 backgroundColor: 'blue',
-//                 borderColor: 'blue',
-//                 borderWidth: 1
-//             }
-//         ]
-//     }
-
-//     var options = {
-//         maintainAspectRatio: false,
-//         scales: {
-//             y: {
-//                 beginAtZero: false,
-//                 min: 14,
-//                 max: 32,
-//                 grid: {
-//                     color: 'rgba(0, 0, 0, 0.1)',
-//                 },
-//                 ticks: {
-//                     stepSize: 2,
-//                 },
-//                 position: 'left', // Ensure the y-axis is on the left side
-//             }
-//         },
-//         legend: {
-//             labels: {
-//                 fontSize: 26
-//             }
-//         }
-//     }
-
-//     return (
-//         <div className='container'>
-//             <div className='top-row'>
-//                 <div className='container-name'>
-//                     <div id='temp-circle'></div>
-//                     <p id='name'>Temperature</p>
-//                 </div>
-//                 <div className='alerts'>
-//                     <div className='alert-max'>
-//                         <div id='alert-max-circle'></div>
-//                         <p>Alert max</p>
-//                     </div>
-//                     <div className='alert-min'>
-//                         <div id='alert-min-circle'></div>
-//                         <p>Alert min</p>
-//                     </div>
-//                     <div className='current'>
-//                         <div id='current'></div>
-//                         <p>Current</p>
-//                     </div>
-//                 </div>
-//             </div>
-//             <br></br>
-//             <div className='graph'>
-//                 <Line
-//                     data={chartData}
-//                     height={330}
-//                     options={options}
-//                 />
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default TempGraph;
