f7f979241f3e44b95151bb13e5aa79b5f9d81004
Diana Antoniuc <diana.antoniuc@gmail.com>
2023-05-20 16:03:29 +0200

    feat: Add registration visual

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import './Login.css'
+import { Modal, Button } from 'react-bootstrap'
+import registration from '../images/registration.png'
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showModal, setShowModal] = useState(false);
 
     const handleUsernameChange = (event) => {
         setUsername(event.target.value);
@@ -21,9 +24,21 @@ const Login = () => {
         });
     };
 
-    const handleReg = () =>{
-        
+    const handleReg = () => {
+        setShowModal(true);
     }
+
+    const handleModalClose = () => {
+        setShowModal(false);
+    }
+
+    const handleRegistration = () => {
+        // Handle reg logic here
+        console.log('Registration form submitted:', {
+            username, password,
+        });
+        handleModalClose();
+    };
 
     return (
         <div className="background">
@@ -55,13 +70,52 @@ const Login = () => {
 
                         <button className="loginButton" type="button" onClick={handleLogin}>
                             Login
-                        </button>                        
+                        </button>
                         <button className="regButton" type="button" onClick={handleReg}>
                             Register
                         </button>
                     </div>
                 </div>
             </div >
+
+            {/* Reg Modal */}
+            <Modal show={showModal} onHide={handleModalClose}>
+                <Modal.Header closeButton>
+                    <img id="reg-logo" src={registration} alt="Registration logo" />
+                    <Modal.Title>Registration</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <label htmlFor="registration-username">
+                        <b>Username:</b>
+                    </label>
+                    <input
+                        type="text"
+                        id="registration-username"
+                        name="registration-username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                    />
+                    <br />
+                    <label htmlFor="registration-password">
+                        <b>Password:</b>
+                    </label>
+                    <input
+                        type="password"
+                        id="registration-password"
+                        name="registration-password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button id='cancel-btn' variant="secondary" onClick={handleModalClose}>
+                        Cancel
+                    </Button>
+                    <Button id='reg-btn' variant="primary" onClick={handleRegistration}>
+                        Register
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div >
     );
 };
