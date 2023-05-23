460523e12314b7a1b847a4d7f0d0d441d3f990f7
Diana Antoniuc <diana.antoniuc@gmail.com>
2023-05-23 16:37:51 +0200

    feat: Add feeding schedule editor modal

--- src/components/FeedingSchedule/feedSchedule.js
+++ src/components/FeedingSchedule/feedSchedule.js
@@ -1,25 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap'
 import foodTray from '../../images/food-tray.png'
 import './feedSchedule.css'
 
 export default function FeedSchedule() {
+  const [datetime, setDatetime] = useState('');
+  const [amount, setAmount] = useState('');
+  const [frequency, setFrequency] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  const handleDateTimeSetup = () => {
+    setDatetime();
+  };
+
+  const handleAmountSetup = () => {
+    setAmount();
+  }
+
+  const handleFrequencySetup = () => {
+    setFrequency();
+  }
+
+  const handleFeederEdit = () => {
+    setShowModal(true);
+  }
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  }
+
+  const handleInputs = () => {
+    // Handle input logic here
+    console.log('Input form submitted:', {
+      datetime,
+      amount,
+      frequency,
+    });
+    handleModalClose();
+  };
+
   return (
     <div className='schedule'>
       <div className='feedingSch'>
         <div className='header'>
-            <img id='feeding-logo' src={foodTray} alt='feedingSch logo'></img>
-            <p id='feedingSch-title'>Feeding schedule</p>
+          <img id='feeding-logo' src={foodTray} alt='feedingSch logo'></img>
+          <p id='feedingSch-title'>Feeding schedule</p>
         </div>
         <div className='details'>
-            <p>Next feeding time - 01/01/2024 12:00</p>
-            <p>Amount - 200g</p>
-            <p>Frequency - every 3 days</p>
+          <p>Next feeding time - 01/01/2024 12:00</p>
+          <p>Amount - 200g</p>
+          <p>Frequency - every 3 days</p>
         </div>
       </div>
       <div className='feedingSchButtons'>
         <button id='feedBttn'><b>Feed</b></button>
-        <button id='editFeedBttn'>Edit schedule</button>
+        <button id='editFeedBttn' type='button' onClick={handleFeederEdit}>Edit schedule</button>
       </div>
+
+      {/* Feeding schedule editor Modal */}
+      <Modal show={showModal} onHide={handleModalClose} centered>
+        <Modal.Header closeButton>
+          <img id='feeding-logo' src={foodTray} alt='feedingSch logo'></img>
+          <Modal.Title>Feeding schedule</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='fields'>
+            <div className='datetime'>
+              <p>Date & time</p>
+              <input
+                type='text'
+                id='datetime'
+                name='datetime'
+                placeholder='dd/mm/yy hh:mm'
+                value={datetime}
+                onChange={handleDateTimeSetup}
+                className='input-field'
+              />
+            </div>
+            <div className='amount'>
+              <p>Amount</p>
+              <input
+                type='text'
+                id='amount'
+                name='amount'
+                placeholder='_____ g'
+                value={amount}
+                onChange={handleAmountSetup}
+                className='input-field'
+              />
+            </div>
+            <div className='frequency'>
+              <p>Frequency</p>
+              <input
+                type='text'
+                id='frequency'
+                name='frequency'
+                placeholder='every _____'
+                value={frequency}
+                onChange={handleFrequencySetup}
+                className='input-field'
+              />
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer className='footerBttns'>
+          <Button id='cancel-btn' variant="secondary" onClick={handleModalClose}>
+            Cancel
+          </Button>
+          <Button id='save-btn' variant="primary" onClick={handleFeederEdit}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
--- src/components/RangeDisplay/RangeDisplay.js
+++ src/components/RangeDisplay/RangeDisplay.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import './RangeDisplay.css';
-import param  from "../../images/param.png";
+import param from "../../images/param.png";
 import axios from 'axios';
 
 function RangeDisplay() {
@@ -46,7 +46,7 @@ function RangeDisplay() {
         minHumidity: humidityRange.min,
         maxHumidity: humidityRange.max
       };
-  
+
       const response = await axios.post(proxyUrl + apiUrl, requestData, {
         method: 'POST',
         headers: {
@@ -71,96 +71,96 @@ function RangeDisplay() {
     <>
       <div className="range-display">
         <div className="temp-ranges">
-        <div  id='t-circle'></div>
-        <p>Temperature range: {temperatureRange.min}°C - {temperatureRange.max}°C</p>
+          <div id='t-circle'></div>
+          <p>Temperature range: {temperatureRange.min}°C - {temperatureRange.max}°C</p>
         </div>
         <div className="co2-ranges">
-        <div  id='c-circle'></div>
-        <p>CO2 range: {co2Range.min}ppm - {co2Range.max}ppm</p>
+          <div id='c-circle'></div>
+          <p>CO2 range: {co2Range.min}ppm - {co2Range.max}ppm</p>
         </div>
         <div className="hum-ranges">
-        <div  id='h-circle'></div>
-        <p>Humidity range: {humidityRange.min}% - {humidityRange.max}%</p>
+          <div id='h-circle'></div>
+          <p>Humidity range: {humidityRange.min}% - {humidityRange.max}%</p>
         </div>
-        <Button id= "edit-parameters" variant="primary" onClick={handleEditParameters}>
+        <Button id="edit-parameters" variant="primary" onClick={handleEditParameters}>
           Edit Parameters
         </Button>
       </div>
 
       <Modal show={showModal} onHide={() => setShowModal(true)} centered>
         <Modal.Header closeButton>
-            <img id="param-logo" src={param} alt= "Parameter logo" />
+          <img id="param-logo" src={param} alt="Parameter logo" />
           <Modal.Title>Edit Parameters</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <div className='parameter-grid'>
-          <div className="parameter-input">
-            <label htmlFor="temperatureMin">Temperature Min:</label>
-            <input
-              type="number"
-              id="temperatureMin"
-              value={temperatureRange.min}
-              onChange={(e) => setTemperatureRange({ ...temperatureRange, min: parseInt(e.target.value) })}
-            />
-          </div>
-          <div className="parameter-input">
-            <label htmlFor="temperatureMax">Temperature Max:</label>
-            <input
-              type="number"
-              id="temperatureMax"
-              value={temperatureRange.max}
-              onChange={(e) => setTemperatureRange({ ...temperatureRange, max: parseInt(e.target.value) })}
-            />
-          </div>
-          <div className="parameter-input">
-            <label htmlFor="co2Min">CO2 Min:</label>
-            <input
-              type="number"
-              id="co2Min"
-              value={co2Range.min}
-              onChange={(e) => setCO2Range({ ...co2Range, min: parseInt(e.target.value) })}
-            />
-          </div>
-          <div className="parameter-input">
-            <label htmlFor="co2Max">CO2 Max:</label>
-            <input
-              type="number"
-              id="co2Max"
-              value={co2Range.max}
-              onChange={(e) => setCO2Range({ ...co2Range, max: parseInt(e.target.value) })}
-            />
-          </div>
-          <div className="parameter-input">
-            <label htmlFor="humidityMin">Humidity Min:</label>
-            <input
-              type="number"
-              id="humidityMin"
-              value={humidityRange.min}
-              onChange={(e) => setHumidityRange({ ...humidityRange, min: parseInt(e.target.value) })}
-            />
-          </div>
-          <div className="parameter-input">
-            <label htmlFor ="humidityMax">Humidity Max:</label>
-            <input
-            type="number"
-            id="humidityMax"
-            value={humidityRange.max}
-            onChange={(e) => setHumidityRange({ ...humidityRange, max: parseInt(e.target.value) })}
-            />
+          <div className='parameter-grid'>
+            <div className="parameter-input">
+              <label htmlFor="temperatureMin">Temperature Min:</label>
+              <input
+                type="number"
+                id="temperatureMin"
+                value={temperatureRange.min}
+                onChange={(e) => setTemperatureRange({ ...temperatureRange, min: parseInt(e.target.value) })}
+              />
             </div>
+            <div className="parameter-input">
+              <label htmlFor="temperatureMax">Temperature Max:</label>
+              <input
+                type="number"
+                id="temperatureMax"
+                value={temperatureRange.max}
+                onChange={(e) => setTemperatureRange({ ...temperatureRange, max: parseInt(e.target.value) })}
+              />
             </div>
-            </Modal.Body>
-            <Modal.Footer>
-            <Button id="cancel-button" variant="secondary" onClick={() => setShowModal(false)}>
+            <div className="parameter-input">
+              <label htmlFor="co2Min">CO2 Min:</label>
+              <input
+                type="number"
+                id="co2Min"
+                value={co2Range.min}
+                onChange={(e) => setCO2Range({ ...co2Range, min: parseInt(e.target.value) })}
+              />
+            </div>
+            <div className="parameter-input">
+              <label htmlFor="co2Max">CO2 Max:</label>
+              <input
+                type="number"
+                id="co2Max"
+                value={co2Range.max}
+                onChange={(e) => setCO2Range({ ...co2Range, max: parseInt(e.target.value) })}
+              />
+            </div>
+            <div className="parameter-input">
+              <label htmlFor="humidityMin">Humidity Min:</label>
+              <input
+                type="number"
+                id="humidityMin"
+                value={humidityRange.min}
+                onChange={(e) => setHumidityRange({ ...humidityRange, min: parseInt(e.target.value) })}
+              />
+            </div>
+            <div className="parameter-input">
+              <label htmlFor="humidityMax">Humidity Max:</label>
+              <input
+                type="number"
+                id="humidityMax"
+                value={humidityRange.max}
+                onChange={(e) => setHumidityRange({ ...humidityRange, max: parseInt(e.target.value) })}
+              />
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button id="cancel-button" variant="secondary" onClick={() => setShowModal(false)}>
             Cancel
-            </Button>
-            <Button id= "save-button" variant="primary" onClick={handleSave}>
+          </Button>
+          <Button id="save-button" variant="primary" onClick={handleSave}>
             Save
-            </Button>
-            </Modal.Footer>
-            </Modal>
-            </>
-            );
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default RangeDisplay;
