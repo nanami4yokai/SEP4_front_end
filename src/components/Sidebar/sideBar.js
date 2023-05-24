791ab59e1c6e3909a37fd543037e626486206399
Diana Antoniuc <diana.antoniuc@gmail.com>
2023-05-24 18:43:36 +0200

    feat: Add terrarium

--- src/components/Sidebar/sideBar.js
+++ src/components/Sidebar/sideBar.js
@@ -1,13 +1,58 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Modal, Button } from 'react-bootstrap'
 import "./Sidebar.css";
 import chameleon from "../../images/chameleon.png";
 import user from "../../images/user.png";
+import plus from '../../images/plus.png'
 
 function Sidebar() {
   const [collapsed, setCollapsed] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState('');
+  const [id, setID] = useState('');
+  const [terrariums, setTerrariums] = useState([
+    { id: 1, name: "Terrarium 1" },
+    { id: 2, name: "Terrarium 2" },
+    { id: 3, name: "Terrarium 3" },
+  ]);
+
   const toggleCollapse = () => {
     setCollapsed(!collapsed);
+  };
+
+  const addTerrarium = () => {
+    setShowModal(true);
+  };
+
+  const handleTerrariumAdd = () => {
+    const newTerrarium = {
+      id: id,
+      name: name
+    };
+    setTerrariums([...terrariums, newTerrarium]);
+    handleModalClose();
+  };
+
+  const handleNameSetup = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleIDSetup = (event) => {
+    setID(event.target.value);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  }
+
+  const handleInputs = () => {
+    // Handle input logic here
+    console.log('Input form submitted:', {
+      name,
+      id,
+    });
+    handleModalClose();
   };
 
   return (
@@ -16,10 +61,10 @@ function Sidebar() {
         {collapsed ? "<<" : ">>"}
       </button>
       <div className="sidebar-content">
-        <img id="logo" src={chameleon} alt="Logo"></img>
+        <img id="logo" src={chameleon} alt="Logo" />
 
         <div className="user">
-          <img id="avatar" src={user} alt="Avatar"></img>
+          <img id="avatar" src={user} alt="Avatar" />
           <div className="user-info">
             <p id="welcome-mssg">Welcome, admin</p>
             <div className="user-info-bttns">
@@ -32,20 +77,61 @@ function Sidebar() {
       <hr id="hr" />
 
       <ul className="sidebar-nav">
-        <li>
-          <a href="/">Terrarium 1</a> {/*<Link to="/terrarium/1">Terrarium 1</Link>*/}
-        </li>
-        <li>
-          <a href="/">Terrarium 2</a>
-        </li>
-        <li>
-          <a href="/">Terrarium 3</a>
-        </li>
+        {terrariums.map((terrarium) => (
+          <li key={terrarium.id}>
+            <Link to={`/terrarium/${terrarium.id}`}>{terrarium.name}</Link>
+          </li>
+        ))}
       </ul>
 
       <div className="add-terrarium">
-        <button id="add-terrarium">+ Add terrarium</button>
+        <button id="add-terrarium" onClick={addTerrarium}>
+          + Add terrarium
+        </button>
       </div>
+
+      {/* Add terrarium Modal */}
+      <Modal show={showModal} onHide={handleModalClose} centered>
+        <Modal.Header closeButton>
+          <img id="plusLogo" src={plus} alt='plus logo'></img>
+          <Modal.Title>Add terrarium</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='fields'>
+            <div className='name'>
+              <p>Name</p>
+              <input
+                type='text'
+                id='name'
+                name='name'
+                value={name}
+                onChange={handleNameSetup}
+                className='input-field'
+              />
+            </div>
+            <div className='id'>
+              <p>ID</p>
+              <input
+                type='number'
+                id='id'
+                name='id'
+                placeholder='Device'
+                value={id}
+                onChange={handleIDSetup}
+                className='input-field'
+              />
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer className='footerBttns'>
+          <Button id='cancel-btn' variant="secondary" onClick={handleModalClose}>
+            Cancel
+          </Button>
+          <Button id='save-btn' variant="primary" onClick={handleTerrariumAdd}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
