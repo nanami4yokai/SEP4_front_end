521611fd52ddc78fd5c6a7550c11c42b8a2bae64
Aiga <aiga.kalneja@gmail.com>
2023-05-29 20:53:28 +0200

    refactor:Update Login APIs + Make config file

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://terrasense-service-dot-terrasense.ew.r.appspot.com/';
+
+export const API_ENDPOINTS = {
+  login: `${API_BASE_URL}/public/login`,
+  register: `${API_BASE_URL}/public/register`,
+  terrariums: `${API_BASE_URL}/terrarium/all`,
+};
